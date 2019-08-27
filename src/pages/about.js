2d48ac7435cb1f68c81b0322307ca37db58a523b
Silcrana <silvana.vieira@hotmail.com>
2019-08-27 18:26:40 -0300

    Added About Page

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const AboutPage = () => (
+  <Layout>
+    <h1>Sobre</h1>
+    <SEO title="Aboult" />
+  </Layout>
+)
+export default AboutPage
