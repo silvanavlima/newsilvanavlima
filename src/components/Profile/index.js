3373dbb131715c1783a83077ac443050cf9d2b41
Silcrana <silvana.vieira@hotmail.com>
2019-08-27 18:26:12 -0300

    Added structure Profile

---
+++ src/components/Profile/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Profile = () => (
+  <div className="Profile-wrapper">
+    <h1>Silvana Lima</h1>
+    <h2>Desenvolvedora Frontend</h2>
+    <p>Sou uma desenvolvedora Frontend.</p>
+  </div>
+)
+
+export default Profile
