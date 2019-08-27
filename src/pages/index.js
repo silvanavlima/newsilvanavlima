192fc5f7990229f27a3857a965ff6e2b6f0132c5
Silcrana <silvana.vieira@hotmail.com>
2019-08-27 18:27:03 -0300

    Added Home Page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -7,6 +8,16 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Home</h1>
+    <ul>
+      <li>
+        <Link to="/" activeStyle={{ color: "blue" }}>
+          Home
+        </Link>
+      </li>
+      <li>
+        <Link to="/about">Sobre</Link>
+      </li>
+    </ul>
   </Layout>
 )
 
